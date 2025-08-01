8a68b603bb26ca4fe160efbd5cd8e0ea252d0b62
Libing Chen <libing.chen@gmail.com>
2025-08-01 11:13:41 +0800

    test: dotenvx_test

---
+++ src/dotenvx_py/tests/dotenvx_test.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+
+from dotenvx_py.dotenvx import (
+    encrypt_item,
+    decrypt_item,
+    load_dotenv,
+    read_pk,
+    read_sk,
+    dotenv_values,
+)
+
+
+def test_encrypt_item():
+    pk_hex = "03053b200bd90daf426d593b1f28e738c7e87487ee45f865f706da320fb7bf0902"
+    sk_hex = "d01f0c48aa665499d995d7769c4bda680992e2549fcd4ba144138d31d176462f"
+    encrypted_text = encrypt_item(pk_hex, "hello")
+    print(encrypted_text)
+    plain_text = decrypt_item(sk_hex, encrypted_text)
+    print(plain_text)
+
+
+def test_load_dotenvx():
+    load_dotenv()
+    print(os.environ["KEY1"])
+
+
+def test_load_values():
+    entries = dotenv_values(".env")
+    print(entries)
+
+
+def test_read_pk():
+    pk = read_pk(".env")
+    print(pk)
+
+
+def test_read_sk():
+    pk = read_sk(None)
+    print(pk)
+    current_dir = Path.cwd()
+    for parent in current_dir.parents:
+        print(parent)
